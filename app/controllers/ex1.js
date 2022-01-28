96294c2b97857e4d21abe5521656e716b79096f0
Le Coupanec Hugo <22003116@etu.unicaen.fr>
2022-01-28 11:59:34 +0100

    ex0.2 debut

--- app/controllers/ex1.js
+++ app/controllers/ex1.js
@@ -1,5 +1,38 @@
 import Controller from '@ember/controller';
-import {action} from '@ember/object';
-import {tracked} from "@glimmer/tracking";
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
-export default class Ex1Controller extends Controller {}
+export default class Ex1Controller extends Controller {
+  MAX = 100;
+  @tracked content = 'Entrez votre texte';
+  @tracked info = '';
+
+  get size() {
+    return this.MAX - this.content.length;
+  }
+
+  get style() {
+    if (this.size < 50) {
+      if (this.size < 20) {
+        return 'danger';
+      }
+      return 'warning';
+    }
+    return 'normal';
+  }
+
+  @action
+  modification() {
+    this.info = 'Note modifiée';
+  }
+
+  @action
+  enregistrement() {
+    this.info = 'Note sauvegardée';
+  }
+
+  @action
+  destruction() {
+    this.info = '';
+  }
+}
