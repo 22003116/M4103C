fd904cfc5d950f8fc95b2c5d3ecd333d8c7bc330
Le Coupanec Hugo <22003116@etu.unicaen.fr>
2022-02-02 16:11:06 +0100

    ex2 3.3

---
+++ app/services/product-services.js
@@ -0,0 +1,20 @@
+import Service from '@ember/service';
+import { dProducts, dPromos } from 'm4103-c/data/datas';
+
+export default class ProductServicesService extends Service {
+  products = dProducts;
+  promos = dPromos;
+
+  get activeServices() {
+    return this.products.filterBy('active', true);
+  }
+
+  get countActive() {
+    return this.activeServices.length;
+  }
+
+  get sumActive() {
+    let call = (total, product) => total + product.price;
+    return this.products.activeServices.reduce(call, 0);
+  }
+}
